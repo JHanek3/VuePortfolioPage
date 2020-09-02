2d413ceef26c277d11dc19b38d5b380b4b53c8b4
Jon <>
2020-09-02 14:44:11 -0500

    Created the Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-main>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Portfolio Website</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  computed: {
+    links() {
+      return this.$store.getters.getLinks;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
