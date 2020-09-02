0840dc72e26369736c74e4a37026e24ac85668ae
Jon <>
2020-09-02 14:19:28 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <Home />
+  </v-app>
+</template>
+
+<script>
+import Home from "@/views/Home.vue";
+export default {
+  name: "App",
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
